1f5b634beb62859eeb029bc90a1c204b7a043f63
Simon Oczadly <oczadly@otris.de>
2025-06-17 22:56:34 +0200

    update

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,97 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 28
+config.window_background_opacity = 0.98
 
 config.font_size = 12
 config.color_scheme = 'GruvboxDark'
 
+config.default_prog = { 'pwsh' }
+config.default_cwd = 'C:\\'
+
+-- Tab bar
+config.window_frame = {
+  font_size = 12.0,
+  active_titlebar_bg = '#333333', -- Not working? maybe windows ...
+  inactive_titlebar_bg = '#313030'
+}
 
 -- Windows stuff
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+-- config.leader = { key = 'space', mods = 'CTRL', timeout_milliseconds = 1000 }
+local act = wezterm.action
+config.keys = {
+
+  -- This will create a new split and run your default program inside it
+  {
+    key = 'h',
+    mods = 'ALT',
+    action = act.SplitPane { direction = 'Left' },
+  },
+  {
+    key = 'j',
+    mods = 'ALT',
+    action = act.SplitPane { direction = 'Down' },
+  },
+  {
+    key = 'k',
+    mods = 'ALT',
+    action = act.SplitPane { direction = 'Up' },
+  },
+  {
+    key = 'l',
+    mods = 'ALT',
+    action = act.SplitPane { direction = 'Right' },
+  },
+  -- change pane size
+  {
+    key = 'h',
+    mods = 'ALT|SHIFT',
+    action = act.AdjustPaneSize { 'Left', 5 },
+  },
+  {
+    key = 'j',
+    mods = 'ALT|SHIFT',
+    action = act.AdjustPaneSize { 'Down', 5 },
+  },
+  {
+    key = 'k',
+    mods = 'ALT|SHIFT',
+    action = act.AdjustPaneSize { 'Up', 5 } },
+  {
+    key = 'l',
+    mods = 'ALT|SHIFT',
+    action = act.AdjustPaneSize { 'Right', 5 },
+  },
+
+  {
+    key = 'f',
+    mods = 'ALT',
+    action = act.PaneSelect
+  },
+  {
+    key = 'd',
+    mods = 'ALT',
+    action = act.CloseCurrentPane { confirm = true }
+  },
+
+  -- Change current tab title
+  {
+    key = 't',
+    mods = 'ALT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+}
 
 return config
